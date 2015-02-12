9e8e925657f612d6a8f735ed2c0ee15236daa0af
Timo Höting <timo@hoeting.eu>
2015-02-12 13:00:25 +0100

    Refactoring der Server-Struktur

---
+++ RaspberryPI/RecvdData.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+################################################
+# Author: Timo Höting       				   #
+# Mail: mail[at]timohoeting.de  			   #
+################################################
+
+import hashlib
+from ConfigReader import *
+
+class RecvdData():
+    def init(self, c):
+        global center
+        center = c
+
+    def dataReceived(self, data):
+        # Protokoll: auth:pw:control:ledNo:rangeStart:rangeEnd:red:green:blue:modus:effectcode:config:hashv
+        # Beispiel: admin:w:X00:1:0:0:10:10:10:0:0:w-w:58acb7acccce58ffa8b953b12b5a7702bd42dae441c1ad85057fa70b
+        # Ermoeglicht Zuweisung von Farben und Effekten
+        # Ermöglicht Abruf von aktuellem Status des Systems und der LEDs
+        #
+        # Ankommende String bei ":" aufsplitten und in Array a[] Speichern:
+        a = data.split(':')
+        print a
+        if len(a) > 1:
+            auth = 		a[0]
+            pw = 		a[1]
+            control = 	a[2]
+            ledNo = 	a[3]
+            rangeStart = a[4]
+            rangeEnd = 	a[5]
+            red = 		a[6]
+            green = 	a[7]
+            blue = 		a[8]
+            modus = 	a[9]
+            effectcode = a[10]
+            config = a[11]
+            hashv = a[12]
+            data = auth + pw + control + ledNo + rangeStart + rangeEnd + red + green + blue + modus + effectcode + config
+            data = data.rstrip('\n')
+            data = data.rstrip('\r')
+            if (self.checkAuthentification(auth, pw) & self.checkTransmissionData(data, hashv)):
+                if control == 'X00':
+                    ## Alle LEDs ausschalten
+                    center.clearPixel()
+                elif control == 'X01':
+                    ## Eine LED anschalten
+                    self.lightUpOneLED(int(ledNo), int(red), int(green), int(blue))
+                elif control == 'X02':
+                    ## LED Bereich anschalten
+                    self.lightUpLEDRange(int(rangeStart), int(rangeEnd), int(red), int(green), int(blue))
+                elif control == 'X03':
+                    ## Eine Farbe für alle LED
+                    self.lightUpAllLED(int(red), int(green), int(blue))
+                elif control == 'X04':
+                    ## Effekt alle LEDs
+                    self.effectLED(effectcode)
+                elif control == 'X05':
+                    ## Modus des Systems
+                    self.changeModus(int(modus))
+                elif control == 'X06':
+                    ## Systemstatus als JSON an den Client
+                    return sendStatus()
+                elif control == 'X07':
+                    ## Status der einzelnen LEDs senden
+                    return sendLEDStatus()
+                elif control == 'X08':
+                    ## Konfiguration ändern
+                    self.changeConfiguration(config)
+            else:
+                print center.writeLog('Übertragung fehlerhaft')
+
+    def changeModus(self, modus):
+        if modus >= 0 & modus < 4:
+            center.setModus(modus)
+
+    def lightUpOneLED(self, ledNo, red, green, blue):
+        # Eine einzelne LED mit den o.g. RGB-Werten dauerhaft anschalten
+        a = self.checkColorRange(red)
+        b = self.checkColorRange(green)
+        c = self.checkColorRange(blue)
+        d = self.checkRange(ledNo)
+        if ( a & b & c & d):
+            center.lightUpOneLED(ledNo, red, green, blue)
+
+    def lightUpLEDRange(self, rangeStart, rangeEnd, red, green, blue):
+        # Einen Bereich von LEDs mit den o.g. RGB-Werten
+        # dauerhaft einschalten
+        # Bereich muss ueberprueft werden mit checkRange()
+        a = self.checkColorRange(red)
+        b = self.checkColorRange(green)
+        c = self.checkColorRange(blue)
+        d = self.checkRange(rangeStart)
+        e = self.checkRange(rangeEnd)
+        if ( a & b & c & d & e):
+            center.rangePixel(rangeStart, rangeEnd, red, green, blue)
+
+    def lightUpAllLED(self, red, green, blue):
+        # Alle LEDs mit den o.g. RGB-Werten
+        # dauerhaft einschalten
+        # Bereich muss ueberprueft werden mit checkRange()
+        a = self.checkColorRange(red)
+        b = self.checkColorRange(green)
+        c = self.checkColorRange(blue)
+        if ( a & b & c):
+            center.lightUpAllLED(red, green, blue)
+
+    def effectLED(self, code):
+        # Effekte auf einer LED aktivieren
+        center.effectLED(code)
+
+    def checkRange(self, ledNo):
+        # Ueberprueft ob die uebergeben LED-Nummer ueberhaupt im
+        # gueltigen Bereich liegt
+        # Es wird der Eintrag 'number' aus dem Config-File geladen
+        reader = ConfigReader()
+        number = int(reader.getNumberOfLED())
+        if ( ledNo >= 0 & ledNo < number):
+            return True
+        else:
+            return False
+
+    def checkColorRange(self, color):
+        # Überprüfung ob Farbwert im gültigen Bereich liegt
+        if (color >= 0 & color <= 255):
+            return True
+        return False
+
+    def checkAuthentification(self, auth, pw):
+        # TODO
+        # Authentifizierung überprüfen
+        # Eingabewert ist das Passwort aus der Übertragung
+        # Dieses wird gehasht und mit dem in der Konfiguration gespeicherten
+        # Hashwert verglichen
+        reader = ConfigReader()
+        hashv = reader.getHashPass()
+        pw = hashlib.sha224(auth).hexdigest()
+        if ( pw == hashv ):
+            return True
+        return False
+
+    def checkTransmissionData(self, data, check):
+        # Korrektheit der Übertragung mittels Hashvergleich feststellen
+        # Eingabewert sind die gesamten Daten der Übertragung
+        hashdata = hashlib.sha224(data).hexdigest()
+        check = check.rstrip('\n')
+        check = check.rstrip('\r')
+        if ( hashdata == check ):
+            return True
+        # TODO Für Testübertragung return immer True
+        return True
+
+    def sendStatus(self):
+        # Status des Systems senden
+        systemstatus = center.getSystemStatus()
+        return systemstatus
+        #self.transport.write(systemstatus.read())
+
+    def sendLEDStatus(self):
+        # Farbwerte aller einzelnen LEDs senden
+        ledstatus = center.getLEDStatus()
+        return ledstatus
+        #self.transport.write(ledstatus.read())
+
+    def changeConfiguration(self, config):
+        b = config.split('--')
+        key = b[0]
+        value = b[1]
+        center.changeConfiguration(key, value)
---
+++ RaspberryPI/ServerSSL.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+################################################
+# Author: Timo Höting       		           #
+# Mail: mail[at]timohoeting.de  		       #
+################################################
+import sys
+import threading
+import hashlib
+from ConfigReader import *
+from OpenSSL import SSL
+from twisted.internet import reactor, ssl
+from twisted.internet.protocol import Factory, Protocol
+
+class LightServerSSL(Protocol):
+    def connectionMade(self):
+        # Es darf nur eine Netzwerkverbindung zum System bestehen
+        # Falls eine weitere aufgebaut wird, so wird sie direkt wieder
+        # gecancelt
+        if (len(connections) >= 1):
+            # TODO: Gibt den Fehler:
+            # 2596:error:140790E5:SSL routines:SSL23_WRITE:ssl
+            # handshake failure:/SourceCache/OpenSSL098/OpenSSL098-52/src/ssl/s23_lib.c:182:
+            self.transport.loseConnection()
+        else:
+            connections.append(self)
+
+    def connectionLost(self, reason):
+        # Wenn die Verbindung getrennt wird, wird die Liste geleert
+        # und die Verbindung im System beendet
+        if self in connections:
+            connections.remove(self)
+
+    def dataReceived(self, line):
+        file = datamanager.dataReceived(data)
+        if (file != None):
+            self.transport.write(file.read())
+
+class StartLightServer(threading.Thread):
+    def __init__(self, c):
+        threading.Thread.__init__(self)
+        global center
+        center = c
+        # TODO: Thread für Datamanager
+        global datamanager
+        datamanager = RecvdData()
+        datamanager.init(center)
+
+    def run(self):
+        global connections
+        connections = []
+        factory = Factory()
+    	factory.protocol = LightServerSSL
+    	reactor.listenSSL(7005, factory, ssl.DefaultOpenSSLContextFactory('./certs/server.key', './certs/server.crt'))
+        # The default reactor, by default, will install signal handlers
+        # to catch events like Ctrl-C, SIGTERM, and so on. However, you can't
+        # install signal handlers from non-main threads in Python, which means
+        # that reactor.run() will cause an error.
+        # Pass the installSignalHandlers=0 keyword argument to reactor.run to
+        # work around this.
+    	reactor.run(installSignalHandlers=False)
