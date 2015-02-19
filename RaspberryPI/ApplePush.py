442b300187ef7641289f4972ddf6771e70fb0df3
Timo Höting <timo@hoeting.eu>
2015-02-19 11:32:12 +0100

    Apple Push Notification mit PyCat APNS

--- RaspberryPI/ApplePush.py
+++ RaspberryPI/ApplePush.py
@@ -5,32 +5,35 @@
 # Mail: mail[at]timohoeting.de  			   #
 ################################################
 
-import socket, ssl, json, struct
-import binascii
+
+import sys
+sys.path.insert(0, 'pycatapns')
+from push import APNS_Push
+from message import APNS_Message_Custom
 from ConfigReader import *
 
 class ApplePush():
     # Das Apple-Device bekommt eine Message und den Namen
     # des neu aufgenommen Fotos gepusht
-    def push(self, message):
-        reader = ConfigReader()
-        deviceToken = reader.getToken()
-        thePayLoad = {
-                     'aps': {
-                          'alert':'Hello world',
-                          'sound':'default',
-                          'badge':42,
-                          },
-                     'test_data': { 'foo': 'bar' },
-                     }
-        theCertfile = 'iphone_ck.pem'
-        theHost = ( 'gateway.sandbox.push.apple.com', 2195 )
-        data = json.dumps( thePayLoad )
-        deviceToken = deviceToken.replace(' ','')
-        byteToken = binascii.unhexlify(deviceToken)
-        theFormat = '!BH32sH%ds' % len(data)
-        theNotification = struct.pack( theFormat, 0, 32, byteToken, len(data), data )
-        ssl_sock = ssl.wrap_socket( socket.socket( socket.AF_INET, socket.SOCK_STREAM ), certfile = theCertfile )
-        ssl_sock.connect( theHost )
-        ssl_sock.write( theNotification )
-        ssl_sock.close()
+    def push(self, message, data):
+        push = APNS_Push(APNS_Push.ENVIRONMENT_SANDBOX, "Studienarbeit-APN.pem")
+        push.setRootCertificationAuthority("Apple.pem")
+        push.connect()
+
+        message = APNS_Message_Custom('xxx')
+        message.setCustomIdentifier("Message-Badge-3")
+        message.setText('')
+        message.setSound()
+        message.setCustomProperty('acme2', ('bang', 'whiz'))
+        message.setExpiry(30)
+        message.setActionLocKey('Show me!')
+        message.setLocKey('Bewegung erkannt! %1$@!')
+        message.setLocArgs(('', 5))
+        message.setLaunchImage('DefaultAlert.png')
+
+        push.add(message)
+        push.send()
+        errors = push.getErrors()
+        if len(errors) > 0:
+        	print(errors)
+        push.disconnect()
